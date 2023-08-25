d87e5743ecf7664350b011899d99db1d043f1a6d
lytoooo0 <lytoooo0@icloud.com>
2023-08-25 23:40:15 +0200

    first version

---
+++ copyurl.js
@@ -0,0 +1,26 @@
+// Create a button element
+var button = document.createElement("button");
+button.textContent = "URL";
+button.id = "floatingButton"; // Assign an ID for styling
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+    var url = window.location.href; // Get the current URL
+    copyToClipboard(url); // Call the function to copy to clipboard
+    sleep(500).then(msg => console.log(msg));
+    button.textContent = "URL";
+});
+
+// Append the button to the body of the document
+document.body.appendChild(button);
+
+// Function to copy text to clipboard
+function copyToClipboard(text) {
+    var tempInput = document.createElement("input");
+    tempInput.value = text;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+    button.textContent = "Copied";
+}
---
+++ tozip.sh
@@ -0,0 +1,1 @@
+zip -r -FS ../copyurl.zip * --exclude *.git* *.sh
